3679b10a87471a61025c43a9fb4854cba7f04f72
ajuan3 <44172655+ajuan3@users.noreply.github.com>
2019-11-14 20:18:16 -0500

    Completed the calculation methods and their Javadoc description.

--- 207Project/src/Main.java
+++ 207Project/src/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * 207 Traveling Salesman Project.
  * 11/14/2019
@@ -10,81 +9,125 @@
  * @author Lucas
  * 
  */
+
 //Create Junit Tests
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		/*In order:
-		Rockville, 
-		Silver Spring, 
-		Philadelphia, 
-		Pittsburgh, 
-		Baltimore, 
-		Cleveland, 
-		New York City 
-		*/
-		final int [][]CITIES_DISTANCES = {
-				{0, 13, 142, 225, 40, 352, 227},
-				{13, 0, 136, 237, 34, 363, 222},
-				{141, 135, 0, 305, 101, 432, 97},
-				{226, 237, 304, 0, 248, 133, 371},
-				{40, 34, 106, 248, 0, 374, 192},
-				{352, 364, 431, 133, 375, 0, 462},
-				{228, 222, 97, 370, 188, 462, 0},
+		final int [][]CITIES_DISTANCES = {				//In order:
+				{0, 13, 142, 225, 40, 352, 227},		//Rockville,
+				{13, 0, 136, 237, 34, 363, 222},		//Silver Spring,
+				{141, 135, 0, 305, 101, 432, 97},		//Philadelphia,
+				{226, 237, 304, 0, 248, 133, 371},		//Pittsburgh,
+				{40, 34, 106, 248, 0, 374, 192},		//Baltimore,
+				{352, 364, 431, 133, 375, 0, 462},		//Cleveland,
+				{228, 222, 97, 370, 188, 462, 0},		//New York City
 				};
 		
 		
+		
+		
+		
+		
 	}
 	
 	
 	
-	public static double hoursOnRoad(){
-		return 0;
+	
+	/**
+	 * Calculate the total time in hours spent on the road
+	 * @param distance total road distance
+	 * @return total time in hours spent on the road
+	 */
+	public static double hoursOnRoad(double distance){
+		return distance *60;
 	}
 	
-	public static double fuelCost(){
-		return 0;
+	/**
+	 * Calculate the total cost of fuel
+	 * @param distance total road distance
+	 * @return total cost of fuel
+	 */
+	public static double fuelCost(double distance){
+		 return (distance/7.0) * 3.32;
 	}
 	
-	public static double driversSalary(){
-		return 0;
+	/**
+	 * Calculate truck driver’s salary
+	 * @param distance total road distance
+	 * @return truck driver’s salary
+	 */
+	public static double driversSalary(double distance){
+		return (distance * 0.56) + 1200;
 	}
 	
-	public static double helpersSalary(){
-		return 0;
+	/**
+	 * Calculate helper's salary
+	 * @param distance total road distance
+	 * @return helper's salary
+	 */
+	public static double helpersSalary(double distance){
+		return (distance * 0.42) + 900;
 	}
 	
-	public static double hotelCost(){
-		return 0;
+	/**
+	 * Calculate the total cost of the hotel
+	 * @param numCities number of cities a single delivery truck has to supply to the supermarkets
+	 * @return total cost of the hotel
+	 */
+	public static double hotelCost(int numCities){
+		return 100.0 * 2 * numCities;
 	}
 	
-	public static double mealCost(){
-		return 0;
+	/**
+	 * Calculate the total cost of meal
+	 * @param numCities number of cities a single delivery truck has to supply to the supermarkets
+	 * @return total cost of meal
+	 */
+	public static double mealCost(int numCities){
+		return 68.0 * 2 * numCities;
 	}
 	
-	public static double maintenanceCost(){
-		return 0;
+	/**
+	 * Calculate the cost of maintenance
+	 * @param distance total road distance
+	 * @return cost of maintenance
+	 */
+	public static double maintenanceCost(double distance){
+		return 0.88 * distance;
 	}
 	
-	public static double totalCostTruck() {
-		return 0;
+	/**
+	 * Calculate the total cost of operating a single delivery truck on a single supply chain mission 
+	 * for supplying to the supermarket stores across the seven cities.
+	 * @param fuel the total cost of fuel
+	 * @param dSalary truck driver’s salary
+	 * @param hSalary helper's salary
+	 * @param hotel total cost of the hotel
+	 * @param meal total cost of meal
+	 * @param maintenance cost of maintenance
+	 * @return total cost of operating a single delivery truck on a single supply chain mission
+	 */
+	public static double totalCostTruck(double fuel, double dSalary, double hSalary, double hotel, double meal, double maintenance) {
+		return fuel + dSalary + hSalary + hotel + meal + maintenance;
 	}
 	
 	public static int totalDistance(int[][] array) {
-		return 0;
+		
+		int totalDistance = 0;
+		
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[i].length; j++) {
+				totalDistance += array[i][j];
+			}
+		}
+		
+		return totalDistance;
 	}
 	
 	public static String cityNames() {
 		return null;
-=======
-
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
->>>>>>> branch 'master' of https://github.com/ajuan3/Great_Wall_Algorithm
 	}
-
 }
